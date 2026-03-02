c9ddeb2122b0830f245bb698cddc7fe4b1ef5ef0
Sophie Hauguel <sophie.hauguel@etu.univ-grenoble-alpes.fr>
2026-03-02 18:24:51 +0100

    Continued Location and Map blocks   => Added code skeleton   => Implemented latlong conversion to meters

--- main.ts
+++ main.ts
@@ -48,12 +48,12 @@ namespace inputSeed {
     }
 
     export class Location {
-        lon: number;
-        lat: number;
+        lat_deg: number; // in degrees
+        lon_deg: number; // in degrees
 
         constructor() {
-            this.lat = 0;
-            this.lon = 0;
+            this.lat_deg = 0;
+            this.lon_deg = 0;
         }
 
         /**
@@ -64,12 +64,36 @@ namespace inputSeed {
         //% this.defl=location
         getLocationElement(typeVal: locationType): number {
             if (typeVal === locationType.locationLat) {
-                return this.lat;
+                return this.lat_deg;
             } else if (typeVal === locationType.locationLon) {
-                return this.lon;
+                return this.lon_deg;
             } else {
                 return NaN;
             }
+        }
+
+        degToRad(degrees : Number) : Number {
+            return degrees * (Math.PI / 180);
+        }
+
+/*
+Formula by Alex Punnen : https://gis.stackexchange.com/a/488625
+meters_per_degree_lat = (111132.92 - 559.82 * np.cos(2 * lat0_rad) +
+                             1.175 * np.cos(4 * lat0_rad) - 0.0023 * np.cos(6 * lat0_rad))
+
+meters_per_degree_lon = (111412.84 * np.cos(lat0_rad) -
+                            93.5 * np.cos(3 * lat0_rad) + 0.118 * np.cos(5 * lat0_rad))
+*/
+        // Returns the equivalent flat coordinates (in 2D meters)
+        export toPoint2D(): Map.Point2D {
+            lat_rad: Number = degToRad(lat);
+
+            lat_m: Number = (111132.92 - 559.82 * Math.cos(2 * lat_rad)
+                + 1.175 * Math.cos(4*lat_rad) - 0.0023 * Math.cos(6*lat_rad));
+            long_m: Number = (111412.84 * Math.cos(lat_rad) - 93.5 * Math.cos(3*lat_rad)
+                + 0.118 * Math.cos(5*lat_rad));
+
+            return new Map.Point2D(long_m, lat_m);
         }
     }
 }
@@ -99,8 +123,10 @@ namespace Map {
     }
 
     export class Map {
-        anchor : Location;
-        // TODO
+        anchor_m : Point2D; // in meters
+        anchorPosition : anchorPositionType;
+        cellSize_m : Number; // in meters
+        points : Point2D[];
 
         //% block="create Map with $anchor on $position and cells measuring $cellSize $sizeUnit"
         export constructor(
@@ -109,8 +135,55 @@ namespace Map {
             cellSize : number,
             sizeUnit : sizeUnitType
             ) {
-            this.anchor = anchor;
-            // TODO : 1° = 111111m
+            this.anchor_m = anchor.toPoint2D;
+            this.anchorPosition = position;
+            if (sizeUnit == sizeUnitType.m) {
+                // sets the size in meters
+                this.cellSize_m = cellSize;
+            } else {
+                // converts the size to meters
+                this.cellSize_m = cellSize*1000;
+            }
+            points = [];
+        }
+
+        export setAnchor(anchor : Location, position : anchorPositionType) {
+            this.anchor_m = anchor.toPoint2D;
+
+        }
+
+        export moveAnchor() {
+
+        }
+
+        export setCellSize() {
+
+        }
+
+        export addLocation() {
+             
+        }
+
+        export print() {
+
+        }
+
+        export clear() {
+
+        }
+
+        export onMove() {
+
+        }
+    }
+
+    export class Point2D {
+        x : Number;
+        y : Number;
+        
+        export constructor(x, y) {
+            this.x = x;
+            this.y = y;
         }
     }
 
