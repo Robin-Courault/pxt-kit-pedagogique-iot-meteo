ea7bf5af2a12c3774b515e0038edcc0ead8dc204
Sophie Hauguel <sophie.hauguel@etu.univ-grenoble-alpes.fr>
2026-03-03 17:25:37 +0100

    Location test started   => corrected compilation errors   => toPoint2D() method access yet to be corrected

--- main.ts
+++ main.ts
@@ -2,6 +2,10 @@
  * Organize your blocks in groups
  */
 
+function degToRad(degrees: number) : number {
+    return degrees * (Math.PI / 180);
+}
+
 //% color="#FFB53D" icon="\uf2ce" weight=110
 //% groups="['Pressure','GPS','Humidity','LoRa']"
 namespace inputSeed {
@@ -72,25 +76,14 @@ namespace inputSeed {
             }
         }
 
-        degToRad(degrees : Number) : Number {
-            return degrees * (Math.PI / 180);
-        }
-
-/*
-Formula by Alex Punnen : https://gis.stackexchange.com/a/488625
-meters_per_degree_lat = (111132.92 - 559.82 * np.cos(2 * lat0_rad) +
-                             1.175 * np.cos(4 * lat0_rad) - 0.0023 * np.cos(6 * lat0_rad))
-
-meters_per_degree_lon = (111412.84 * np.cos(lat0_rad) -
-                            93.5 * np.cos(3 * lat0_rad) + 0.118 * np.cos(5 * lat0_rad))
-*/
+        //Formula by Alex Punnen : https://gis.stackexchange.com/a/488625
         // Returns the equivalent flat coordinates (in 2D meters)
-        export toPoint2D(): Map.Point2D {
-            lat_rad: Number = degToRad(lat);
+        toPoint2D(): Map.Point2D {
+            const lat_rad: number = degToRad(this.lat_deg);
 
-            lat_m: Number = (111132.92 - 559.82 * Math.cos(2 * lat_rad)
+            const lat_m: number = (111132.92 - 559.82 * Math.cos(2 * lat_rad)
                 + 1.175 * Math.cos(4*lat_rad) - 0.0023 * Math.cos(6*lat_rad));
-            long_m: Number = (111412.84 * Math.cos(lat_rad) - 93.5 * Math.cos(3*lat_rad)
+            const long_m: number = (111412.84 * Math.cos(lat_rad) - 93.5 * Math.cos(3*lat_rad)
                 + 0.118 * Math.cos(5*lat_rad));
 
             return new Map.Point2D(long_m, lat_m);
@@ -125,17 +118,17 @@ namespace Map {
     export class Map {
         anchor_m : Point2D; // in meters
         anchorPosition : anchorPositionType;
-        cellSize_m : Number; // in meters
+        cellSize_m : number; // in meters
         points : Point2D[];
 
         //% block="create Map with $anchor on $position and cells measuring $cellSize $sizeUnit"
-        export constructor(
+        constructor(
             anchor : Location, 
             position : anchorPositionType,
             cellSize : number,
             sizeUnit : sizeUnitType
             ) {
-            this.anchor_m = anchor.toPoint2D;
+            this.anchor_m = anchor.toPoint2D();
             this.anchorPosition = position;
             if (sizeUnit == sizeUnitType.m) {
                 // sets the size in meters
@@ -144,48 +137,51 @@ namespace Map {
                 // converts the size to meters
                 this.cellSize_m = cellSize*1000;
             }
-            points = [];
+            this.points = [];
         }
 
-        export setAnchor(anchor : Location, position : anchorPositionType) {
-            this.anchor_m = anchor.toPoint2D;
-
-        }
-
-        export moveAnchor() {
+        setAnchor(anchor : Location, position : anchorPositionType) {
+            this.anchor_m = anchor.toPoint2D();
 
         }
 
-        export setCellSize() {
+        moveAnchor() {
 
         }
 
-        export addLocation() {
+        setCellSize() {
+
+        }
+
+        addLocation() {
              
         }
 
-        export print() {
+        print() {
 
         }
 
-        export clear() {
+        clear() {
 
         }
 
-        export onMove() {
+        onMove() {
 
         }
     }
 
     export class Point2D {
-        x : Number;
-        y : Number;
+        x : number;
+        y : number;
         
-        export constructor(x, y) {
+        constructor(x, y) {
             this.x = x;
             this.y = y;
         }
     }
 
 }
+
+const loc = new Location();
+console.log(loc.getLocationElement(LocationType.locationLon));
 
